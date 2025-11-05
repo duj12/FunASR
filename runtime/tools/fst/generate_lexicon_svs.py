ec899501408223e0795f2208f16221960c32983a
dujing <dujing@xmov.ai>
2025-11-05 17:06:27 +0800

    add script to build ngram and TLG.fst for SenseVoice model's token units.

---
+++ runtime/tools/fst/generate_lexicon_svs.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+import json
+
+# input
+# sys.argv[1]: lm dict, with all words in the corpus
+# sys.argv[2]: token unit file
+# sys.argv[3]: bpe model file
+# output
+# sys.argv[4]: lexicon file for corpus.dict, map raw words to token units
+# sys.argv[5]: units file output, assign each unit an integer id
+# sys.argv[6]: units json file output, add <blank> token at the beginning  
+
+def contain_oov(units):
+    for unit in units:
+        if unit not in unit_table:
+            return True
+    return False
+
+# step 1: load unit table
+unit_table = []
+with open(sys.argv[2], 'r', encoding='utf8') as fin:
+    unit_table = json.load(fin)
+    if '<blank>' not in unit_table:
+        unit_table = ['<blank>'] + unit_table
+print(f"unit size: {len(unit_table)}")
+
+# step 2: load sentence piece model
+import sentencepiece as spm
+sp = spm.SentencePieceProcessor()
+sp.Load(sys.argv[3])
+
+# convert the unit table to a output format, units.txt and units.json    
+with open(sys.argv[5], 'w', encoding='utf8') as fout:
+    for idx, unit in enumerate(unit_table):
+        fout.write(f"{unit} {idx}\n")
+with open(sys.argv[6], 'w', encoding='utf-8') as fout:
+    json.dump(unit_table, fout, ensure_ascii=False, indent=4)
+
+
+lexicon_table = set()
+with open(sys.argv[1], 'r', encoding='utf8') as fin, \
+        open(sys.argv[4], 'w', encoding='utf8') as fout:
+    for line in fin:
+        word = line.split()[0]
+        if word == 'SIL':  # `sil` might be a valid piece in bpemodel
+            continue
+        elif word == '<SPOKEN_NOISE>':
+            continue
+        elif word == "<s>" or word == "</s>" or word=="<eps>":
+            continue
+        elif word == '<unk>':
+            fout.write('{}\t{}\n'.format(word, word))
+            lexicon_table.add(word)
+        else:
+            # each word only has one pronunciation for e2e system
+            if word in lexicon_table:
+                continue
+            pieces = sp.EncodeAsPieces(word)
+            if contain_oov(pieces):
+                print(
+                    'Ignoring words {}, which contains oov unit, piece {}'.format(
+                        ''.join(word).strip('▁'), pieces)
+                )
+                # continue
+            chars = ' '.join(
+                [p if p in unit_table else '<unk>' for p in pieces])
+            
+            fout.write('{}\t{}\n'.format(word, chars))
+            lexicon_table.add(word)
+
+print(f"lexicon size: {len(lexicon_table)}")
--- runtime/tools/path.sh
+++ runtime/tools/path.sh
@@ -1,3 +1,6 @@
 export FUNASR_RUNTIME_ROOT=`pwd`/../
 export PATH=$FUNASR_RUNTIME_ROOT/onnxruntime/build/bin:$PWD:$PATH
+export PATH=/data/megastore/Projects/DuJing/code/kaldi/tools/srilm/bin:/data/megastore/Projects/DuJing/code/kaldi/tools/srilm/bin/i686-m64:$PATH
+export PATH=/data/megastore/Projects/DuJing/code/FunASR-main/runtime/websocket/build/bin:$PATH
+
 export LC_ALL=C
